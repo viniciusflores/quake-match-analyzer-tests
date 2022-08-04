81a68ca759a58fdb1075247d9945bf02c0401b64
Vinicius Flores <viniciusflores379@gmail.com>
2022-08-04 15:03:53 -0300

    :sparkles: Include test to GET /games

--- src/tests/api/allGames.spec.js
+++ src/tests/api/allGames.spec.js
@@ -1,10 +1,31 @@
 import { expect } from 'chai';
+import got from 'got';
 
-describe('Suite tests', function () {
-  it('this is a test', function () {
-    const a = 1;
-    const b = 2;
+let baseUrl;
+describe('Suite tests of GET in /games', function () {
+  before(function () {
+    baseUrl = process.env.API_URL;
+  });
 
-    expect(a + b).to.equal(3);
+  it('Should be possible to list all games', async function () {
+    try {
+      const { statusCode, statusMessage, body, headers } = await got.get(
+        `${baseUrl}/games`,
+        {
+          responseType: 'json',
+        },
+      );
+
+      expect(statusCode).to.equal(200);
+      expect(statusMessage).to.equal('OK');
+      expect(headers['content-type']).to.equal(
+        'application/json; charset=utf-8',
+      );
+      expect(body).to.not.equal(null);
+      expect(body).to.include.any.keys('game_1', 'game_2');
+    } catch (err) {
+      console.error('FAILED on CATCH of: this is a api test');
+      console.error(`ERROR: ${err}`);
+    }
   });
 });
